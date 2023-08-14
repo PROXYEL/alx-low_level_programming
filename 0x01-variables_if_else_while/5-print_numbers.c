6a5af78fa479201cce8f513fb28b24c051bc7e7d
PROXYEL <awesomeel452@gmail.com>
2023-08-14 07:35:23 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print decimals
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+	char d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+
+	return (0);
+}
