8c45246d7eca42e77c2485b3300750bde1cf644e
PROXYEL <awesomeel452@gmail.com>
2023-08-14 07:18:02 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets expect q and e
+ *
+ * Return: Always (0)
+ *
+ */
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		if (i != 'e' && i != 'q')
+		{
+		putchar(i);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
