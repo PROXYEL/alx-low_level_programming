0d8b86084d5edc063e66bd6db78d976c2524626c
PROXYEL <awesomeel452@gmail.com>
2023-08-14 08:53:39 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -
+ * print decimal with, and space
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+	char d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+		if (d <= '8')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+
+		return (0);
+}
