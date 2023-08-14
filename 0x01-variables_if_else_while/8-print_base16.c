d853e896fcfa5a127877cd8e1119dac760a7043e
PROXYEL <awesomeel452@gmail.com>
2023-08-14 08:04:46 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print base sixteen
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+	char n;
+	char t = 'a';
+
+	for (n '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	for (t = 'a'; t <= 'f'; t++)
+	{
+		putchar(t);
+	}
+	putchar('\n');
+
+	return (0);
