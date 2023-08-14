5012457e4cd8e6348d084fe35db55159b4accf35
PROXYEL <awesomeel452@gmail.com>
2023-08-14 11:24:14 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - print digits in 2s separated by ,
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+	int i, j;
+	int a, b, c, d;
+
+	for (i = '0'; i <= 99; i++)
+	{
+		a = i / 10;
+		b = i % 10;
+
+		for (j = '0'; j <= 99; j++)
+		{
+			c = j / 10;
+			d = j % 10;
+			if (a < c || (a == c && b < d))
+			{
+				putchar(a);
+				putchar(b);
+				putchar(' ');
+				putchar(c);
+				putchar(d);
+
+				if (a != '9' && b != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
