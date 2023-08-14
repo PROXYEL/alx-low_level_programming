7fcd12e351c73ddd35196dc4bad3994ac79fd9f8
PROXYEL <awesomeel452@gmail.com>
2023-08-14 10:54:22 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - print 3 digits seperated by , and space
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		j = '0';
+		while (j <= '9')
+		{
+			k = '0'
+			while (k <= '9')
+			{
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != '7')
+					{
+					putchar(',')
+					putchar(' ');
+					}
+
+				}
+				k++;
+			}
+			j++;
+		}
+	}
+	putchar('\n')
+
+	return (0);
+}
