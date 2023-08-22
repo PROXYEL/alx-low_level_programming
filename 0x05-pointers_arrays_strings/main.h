3c9b38adaf019cec1492d6efdb021c4994dc4338
PROXYEL <awesomeel452@gmail.com>
2023-08-21 21:36:43 -0700

    0-reset_to_98

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,24 +1,23 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x04-pointers_arrays_strings directory.
- */
-
-#include <stdlib.h>
-
 int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int size_t_strlen(const char *str);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
 
-#endif
+void reset_to_98(int *n);
+
+void swap_int(int *a, int *b);
+
+int _strlen(char *s);
+
+void _puts(char *str);
+
+void print_rev(char *s);
+
+void rev_string(char *s);
+
+void put2(char *str);
+
+void puts_half(char *str);
+
+void print_array(int *a, int n);
+
+char *_strcpy9( char *dest, char *src);
+
+int _atoil(char *s);
