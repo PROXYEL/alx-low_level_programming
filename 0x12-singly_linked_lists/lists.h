f03479d172761e4c04cafc5927c8e3a2cd27a82f
PROXYEL <awesomeel452@gmail.com>
2023-09-22 14:55:41 -0700

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
-#include <stdlib.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -18,7 +17,6 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-int _putchar(char c);
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
