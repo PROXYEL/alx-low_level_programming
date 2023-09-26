cced29443d0b5e520ec33f4ad1e65fef891b0eac
PROXYEL <awesomeel452@gmail.com>
2023-09-26 08:56:52 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,33 +11,29 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
-	listint_t *temp, *new;
+	unsigned int count = 0;
+	listint_t *new_node, *temp = *head;
 
 	if (head == NULL)
 		return (NULL);
-	if (idx != 0)
-	{
-		tmp = *head;
-		for (i = 0; i < idx - 1 && temp != NULL; i++)
-		{
-			temp = temp->next;
-		}
-		if (temp == NULL)
-			return (NULL);
-	}
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
 		return (NULL);
-	}
-	new->n = n;
+
+	new_node->n = n;
 	if (idx == 0)
 	{
-		new->next = *head;
-		*head = new;
-		return (new);
+		new_node->next = temp;
+		*head = new_node;
+		return (new_node);
 	}
-	new->next = temp->next;
-	temp->next = new;
-	return (new);
+	for (; count < (idx - 1); count++)
+	{
+		if (temp == NULL || temp->next == NULL)
+			return (NULL);
+		temp = temp->next;
+	}
+	new_node->next = temp->next;
+	temp->next = new_node;
+	return (new_node);
 }
